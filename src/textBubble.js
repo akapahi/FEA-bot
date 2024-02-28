8d62c92136df2d9fb18baac0ee23cd14f3f5c072
Abhishek Kapahi <54096646+akapahi@users.noreply.github.com>
2024-02-28 22:21:42 +0530

    Update textBubble.js

--- src/textBubble.js
+++ src/textBubble.js
@@ -1,32 +1,68 @@
 import React from "react";
 
-const TextBubble = ({ userText, apiResponse }) => (
-  <div>
-    {userText && (
+const TextBubble = ({ userText, apiResponse }) => {
+  const extractURLs = (text) => {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.split(urlRegex);
+  };
+
+  const parts = extractURLs(apiResponse);
+
+  return (
+    <div>
+      {userText && (
+        <div
+          style={{
+            border: "1px solid #ccc",
+            borderRadius: "20px",
+            padding: "2px 10px",
+            marginTop: "10px",
+            textAlign: "left",
+            background: "#5887F5",
+            color: "white",
+            float: "right",
+            clear: "left",
+            maxWidth: "60vw",
+            //minWidth: "30vw",
+          }}
+        >
+          <p>{userText}</p>
+        </div>
+      )}
       <div
         style={{
+          textAlign: "left",
+          borderRadius: "20px",
+          padding: "2px 10px",
           border: "1px solid #ccc",
-          borderRadius: "10px",
-          padding: "10px",
           marginTop: "10px",
-          textAlign: "right",
+          background: "lightgray",
+          maxWidth: "60vw",
+          minWidth: "30vw",
+          float: "left",
+          clear: "right",
         }}
       >
-        <p>{userText}</p>
+        <p style={{ whiteSpace: "pre-wrap" }}>
+
+          {parts.map((part, index) =>
+            part.match(/https?:\/\//) ? (
+              <a
+                key={index}
+                href={part}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {part}
+              </a>
+            ) : (
+              part
+            )
+          )}
+        </p>
       </div>
-    )}
-    <div
-      style={{
-        textAlign: "left",
-        borderRadius: "10px",
-        border: "1px solid #ccc",
-        padding: "10px",
-        marginTop: "10px",
-      }}
-    >
-      <p>{apiResponse}</p>
     </div>
-  </div>
-);
+  );
+};
 
 export default TextBubble;
